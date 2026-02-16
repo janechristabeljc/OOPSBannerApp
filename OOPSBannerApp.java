218c91eee8a682398f89ffad402444c9f75af5eb
Jane Christabel <janechristabel.jc@gmail.com>
2026-02-16 14:16:02 +0530

    UC1: Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp
+ *
+ * This program prints OOPS to the console.
+ *
+ * @author Jane Christabel Wesley Paul
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
