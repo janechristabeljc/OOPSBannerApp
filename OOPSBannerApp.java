9f316ef9bba4705bae0bfd72dbfe71d18004d90c
Jane Christabel <janechristabel.jc@gmail.com>
2026-02-23 13:17:56 +0530

    UC4: render OOPS banner using String array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,19 @@
-/**
- * OOPSBannerApp
- *
- * UC2: Print OOPS in banner format using print statements
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" ***     " + " ***     " + " ****** " + " ***** ");
-        System.out.println("** **    " + "** **    " + "**   ** " + "**   **");
-        System.out.println("**  **   " + "**  **   " + "**   ** " + "**   **");
-        System.out.println("**  **   " + "**  **   " + "******  " + " ***** ");
-        System.out.println("**  **   " + "**  **   " + "**      " + "     **");
-        System.out.println("** **    " + "** **    " + "**      " + "**   **");
-        System.out.println(" ***     " + " ***     " + "**      " + " ***** ");
+        String[] banner = {
+            " ***      ***   ******   ***** ",
+            "**  **   **  **  **   ** **    ",
+            "**  **   **  **  **   ** **    ",
+            "**  **   **  **  ******   ***  ",
+            "**  **   **  **  **           **",
+            "**  **   **  **  **           **",
+            " ***      ***   **       ***** "
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
