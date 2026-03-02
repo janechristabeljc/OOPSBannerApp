7915226b793eef30bda6f8f589c39fc2ff1942d7
Jane Christabel <janechristabel.jc@gmail.com>
2026-03-02 13:01:39 +0530

    UC6: refactor banner using static helper methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,18 +2,48 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            " ***      ***   ******   ***** ",
-            "**  **   **  **  **   ** **    ",
-            "**  **   **  **  **   ** **    ",
-            "**  **   **  **  ******   ***  ",
-            "**  **   **  **  **           **",
-            "**  **   **  **  **           **",
-            " ***      ***   **       ***** "
-        };
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < 7; i++) {
+            System.out.println(o[i] + "   " + o[i] + "   " + p[i] + "   " + s[i]);
         }
+    }
+
+    static String[] getOPattern() {
+        return new String[] {
+            " ***** ",
+            "**   **",
+            "**   **",
+            "**   **",
+            "**   **",
+            "**   **",
+            " ***** "
+        };
+    }
+
+    static String[] getPPattern() {
+        return new String[] {
+            "****** ",
+            "**   **",
+            "**   **",
+            "****** ",
+            "**     ",
+            "**     ",
+            "**     "
+        };
+    }
+
+    static String[] getSPattern() {
+        return new String[] {
+            " ***** ",
+            "**     ",
+            "**     ",
+            " ***** ",
+            "     **",
+            "     **",
+            " ***** "
+        };
     }
 }
